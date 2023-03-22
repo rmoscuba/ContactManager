caa04dfb0228d3d5944b1c566f9ca417edb3e9d3
Rodolfo Ortega <rmoscuba@gmail.com>
2023-03-22 02:04:41 -0400

    Add User Model

---
+++ ContactManager/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ContactManager.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [StringLength(128)]
+        [Required]
+        public string FirstName { get; set; }
+
+        [StringLength(128)]
+        [Required]
+        public string LastName { get; set; }
+
+        [StringLength(60)]
+        [Required]
+        public string UserName { get; set; }
+
+        [StringLength(256)]
+        [Required]
+        public string PassWord { get; set; }
+
+    }
+}
