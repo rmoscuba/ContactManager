a86e6b9eb7d7507aa0dd6d4b118a66536ae04b2c
Rodolfo Ortega <rmoscuba@gmail.com>
2023-03-22 02:00:18 -0400

    Add Contact Model

---
+++ ContactManager/Models/Contact.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ContactManager.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("Contact")]
+    public class Contact
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [StringLength(128)]
+        [Required]
+        public string FirstName { get; set; }
+
+        [StringLength(128)]
+        public string LastName { get; set; }
+
+        [StringLength(128)]
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [StringLength(20)]
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public Guid Owner { get; set; }
+
+    }
+}
