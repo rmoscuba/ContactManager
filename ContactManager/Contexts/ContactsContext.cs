2e0696c2941e3056ac8f60b5c082565e98f0a15b
Rodolfo Ortega <rmoscuba@gmail.com>
2023-03-22 02:19:32 -0400

    Add DB Context

---
+++ ContactManager/Contexts/ContactsContext.cs
@@ -0,0 +1,15 @@
+﻿namespace ContactManager.Contexts
+{
+    using ContactManager.Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ContactsContext: DbContext
+    {
+        public ContactsContext(DbContextOptions options): base(options) {}
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+    }
+}
